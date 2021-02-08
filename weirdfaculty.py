7c42185d3b909db980df9a2311d57d945c43bbbb
Sanjay Nithin <71659658+SanjayNithin2002@users.noreply.github.com>
2021-02-08 22:21:01 +0530

    Add files via upload

---
+++ weirdfaculty.py
@@ -0,0 +1,16 @@
+def exam(v):
+    for k in range(len(v)):
+        b,c=0,0
+        for i in range(k):
+            if v[i]==1:b+=1
+            elif v[i]==0:b-=1
+        for j in range(k,len(v)):
+                if v[j]==1:c+=1
+                elif v[j]==0:c-=1
+        if b>c:
+                return k
+
+v=list()
+for i in range(int(input())): v.append(int(input()))
+print(exam(v))
+
